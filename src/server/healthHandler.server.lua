121f6737b24ce27cd8c98caef3e203d0ae0bf702
chavez <95760172+spumdonut@users.noreply.github.com>
2022-03-11 22:19:29 -0500

    add stuff

--- src/server/healthHandler.server.lua
+++ src/server/healthHandler.server.lua
@@ -4,10 +4,13 @@ end
 
 local canHurt = true
 workspace.Part.Touched:Connect(function(otherPart)
-    if canHurt then return end
+    if not canHurt then return end
 
-    if not canHurt then
-        takeHealth(game.Players:GetPlayerFromCharacter(otherPart.Parent))
+    if canHurt then
+        local player = game.Players:GetPlayerFromCharacter(otherPart.Parent)
+        takeHealth(player)
+        print('MESSAGE: took 25 health from '.. player.Name .. '.' .. '  2 seconds until next hurt!')
+        print()
         canHurt = false
         wait(2)
         canHurt = true
