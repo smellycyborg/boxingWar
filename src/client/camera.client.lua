86cf9f7dd4d6010affffe0486577b7b34d6e5088
chavez <95760172+spumdonut@users.noreply.github.com>
2022-03-13 15:45:51 -0400

    add camera

---
+++ src/client/camera.client.lua
@@ -0,0 +1,19 @@
+local player = game.Players.LocalPlayer 
+local character = player.Character or player.CharacterAdded:wait()
+
+local fieldOfView = 75
+local offset = Vector3.new(-5 ,35, 10)
+local camera = workspace.CurrentCamera
+
+local runService = game:GetService('RunService')
+
+
+function onRenderStep()
+    camera.FieldOfView = fieldOfView
+    camera.CameraType = Enum.CameraType.Scriptable
+    local playerPosition = character.HumanoidRootPart.Position
+    local cameraPosition = playerPosition + offset
+    camera.CoordinateFrame = CFrame.new(cameraPosition, playerPosition)
+end
+
+runService:BindToRenderStep('Camera', Enum.RenderPriority.Camera.Value, onRenderStep)
