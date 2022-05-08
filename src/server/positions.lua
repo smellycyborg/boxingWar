b47fe532ecc4e79452fd69b6409735044d5360f0
chavez <chavez.swagger@gmail.com>
2022-05-08 16:00:39 -0400

    edit positionfy in positions module

--- src/server/positions.lua
+++ src/server/positions.lua
@@ -1,11 +1,17 @@
 local positions = {}
 
 function positions.positionfy(object)
+    local position
     local xRange = math.random(-0.125, 30)
     local zRange = math.random(2, 50)
     local yRange = 5.5
-    local position = Vector3.new(xRange, yRange, zRange)
-    return position
+    position = Vector3.new(xRange, yRange, zRange)
+    if table.find(positions, position) then
+        positions.positionfy(object)
+    else
+        table.insert(positions, position)
+        return position
+    end
 end
 
 return positions
