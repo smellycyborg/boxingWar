6f8d8fc0556ba439d16305a988a4d92b961c844d
chavez <chavez.swagger@gmail.com>
2022-05-20 22:28:52 -0400

    refined craft handler

--- src/client/craftHandler.client.lua
+++ src/client/craftHandler.client.lua
@@ -3,13 +3,12 @@ local CraftingGui = PlayerGui:WaitForChild('CraftingGui')
 local CraftingButtonsHolder = CraftingGui.CraftingFrame
 local CraftEvent = game.ReplicatedStorage.KLEvents.CraftEvent
 
-local function onClick()
-    local toCraft = 'StoneAxe'
-    CraftEvent:FireServer(toCraft)
-end
-
 for _, craftButton in pairs(CraftingButtonsHolder:GetChildren()) do
     if craftButton:IsA('TextButton') then
+        local function onClick()
+            local toCraft = craftButton.Name
+            CraftEvent:FireServer(toCraft)
+        end
         craftButton.MouseButton1Down:Connect(onClick)
     end
 end
